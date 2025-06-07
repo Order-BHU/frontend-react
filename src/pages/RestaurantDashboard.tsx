d25a73025826e94626cd63f6e9f47c0152d2f520
Taterbro <veecipher@gmail.com>
2025-06-07 10:10:33 +0100

    wallet balance

--- src/pages/RestaurantDashboard.tsx
+++ src/pages/RestaurantDashboard.tsx
@@ -511,7 +511,7 @@ const RestaurantDashboardPage = () => {
               </div>
             </div>
             <div className="text-2xl font-bold text-secondary-900">
-              ₦{Number(userDetails?.earnings?.total).toLocaleString()}
+              ₦{Number(userDetails?.wallet_balance).toLocaleString()}
             </div>
           </div>
 
