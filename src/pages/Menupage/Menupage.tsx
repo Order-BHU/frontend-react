5454a181546fae06198e6e4755eac2c854316c57
Taterbro <veecipher@gmail.com>
2025-07-12 09:34:20 +0100

    Update Menupage.tsx

--- src/pages/Menupage/Menupage.tsx
+++ src/pages/Menupage/Menupage.tsx
@@ -358,7 +358,7 @@ const RestaurantMenuPage = () => {
     const now = new Date();
     const hour = now.getHours();
 
-    if (hour >= 12 && hour < 19) {
+    if (hour >= 14 && hour < 20) {
       setIsAllowedTime(true);
     } else {
       setIsAllowedTime(false);
